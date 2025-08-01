0958e019743bc4bc2635a719fa07c9e54be8eeb1
tsisam967 <23jesmari@gmail.com>
2025-08-01 10:01:24 -0400

    Add files via upload

---
+++ backend/routes/projectRoutes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/authMiddleware');
+const {
+  createProject,
+  getProjects,
+  updateProject,
+  deleteProject
+} = require('../controllers/projectController');
+
+router.post('/', auth, (req, res, next) => {
+  console.log('[POST] /projects accessed');
+  next();
+}, createProject);
+
+router.get('/', auth, (req, res, next) => {
+  console.log('[GET] /projects accessed');
+     next();
+}, getProjects);
+
+router.put('/:id', auth, (req, res, next) => {
+  console.log('[PUT] /projects/:id accessed');
+  next();
+}, updateProject);
+
+router.delete('/:id', auth, (req, res, next) => {
+  console.log('[DELETE] /projects/:id accessed');
+  next();
+}, deleteProject);
+
+module.exports = router;
