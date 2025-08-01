93f0be0a20998846f68d2299d2106731f0d72ddd
tsisam967 <23jesmari@gmail.com>
2025-08-01 10:35:18 -0400

    Add files via upload

---
+++ backend/controllers/authController.js
@@ -0,0 +1,23 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+exports.signup = async (req, res) => {
+  const { email, password } = req.body;
+  const hashed = await bcrypt.hash(password, 10);
+  const user = new User({ email, password: hashed });
+  await user.save();
+  res.json({ message: 'User created' });
+};
+
+exports.signin = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) return res.status(400).json({ message: 'Invalid email' });
+
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) return res.status(400).json({ message: 'Invalid password' });
+
+  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+  res.json({ token });
+};
