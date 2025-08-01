3e1eaeb3fe22726a545a17b97d0817e0ac8e299e
tsisam967 <23jesmari@gmail.com>
2025-07-31 23:18:08 -0400

    Update authRoutes.js

--- backend/authRoutes.js
+++ backend/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { signup, signin } = require('../authController');
+const { signup, signin } = require('../controllers/authController');
 
 router.post('/signup', signup);
 router.post('/signin', signin);
